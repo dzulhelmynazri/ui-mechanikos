aed80aea9ab9dd0b6fc27d5dc65ad1eee3acf6c9
Dzulhelmy Bin Nazri <trial.dzulhelmy@gmail.com>
2025-07-03 14:51:31 +0800

    first commit

---
+++ lib/config.ts
@@ -0,0 +1,38 @@
+export const siteConfig = {
+  name: "Mechanikos UI",
+  url: "https://mechanikos.com",
+  ogImage: "https://mechanikos.com/og.jpg",
+  description:
+    "A set of beautifully-designed, accessible components and a code distribution platform.",
+  links: {
+    twitter: "https://twitter.com/mechanikos",
+    github: "https://github.com/mechanikos",
+  },
+  navItems: [
+    {
+      href: "/docs",
+      label: "Docs",
+    },
+    {
+      href: "/docs/components",
+      label: "Components",
+    },
+    {
+      href: "/blocks",
+      label: "Blocks",
+    },
+    {
+      href: "/charts/area",
+      label: "Charts",
+    },
+    {
+      href: "/themes",
+      label: "Themes",
+    },
+  ],
+}
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+}
