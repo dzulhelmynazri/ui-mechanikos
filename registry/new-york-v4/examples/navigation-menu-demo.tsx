1b9d0cae84e0e9a88e97333337d2095ce0c0e87c
Dzulhelmy <trial.dzulhelmy@gmail.com>
2025-07-30 17:27:01 +0800

    Push

--- registry/new-york-v4/examples/navigation-menu-demo.tsx
+++ registry/new-york-v4/examples/navigation-menu-demo.tsx
@@ -67,7 +67,7 @@ export default function NavigationMenuDemo() {
                     href="/"
                   >
                     <div className="mt-4 mb-2 text-lg font-medium">
-                      shadcn/ui
+                      Mechanikos/UI
                     </div>
                     <p className="text-muted-foreground text-sm leading-tight">
                       Beautifully designed components built with Tailwind CSS.
